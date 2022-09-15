f219619929b6e2295749c5c14b6d786208a06064
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 <66234273+yappeizhen@users.noreply.github.com>
2022-09-16 00:45:20 +0800

    Fixed merge conflicts

--- codeitsuisse/routes/ticker_stream.py
+++ codeitsuisse/routes/ticker_stream.py
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logging
-import json
-
-from flask import request, jsonify
-
-from codeitsuisse import app
-
-logger = logging.getLogger(__name__)
-
-@app.route('/tickerStreamTest', methods=['GET'])
-def test():
-    return "Hello, ticker stream is working"
-
-@app.route('/tickerStreamPart1', methods=['POST'])
-def ticker_stream_part_1():
-    data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
-    return json.dumps(result)
-=======
 import logging
 import json
 
@@ -51,4 +28,3 @@ def ticker_stream_part_2():
     data = ticker_stream_challenge.to_cumulative_delayed(
         json.loads(stream), int(qty_block))
     return jsonify(data)
->>>>>>> 06cc16fab376b0c10a5cbb7a8bc88b2477a75d8d
