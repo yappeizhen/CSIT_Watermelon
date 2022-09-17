d16cb2eba9a386d5e70178255d4a7ebf397ab500
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 <66234273+yappeizhen@users.noreply.github.com>
2022-09-17 10:14:59 +0800

    Crypto Collapz API and logic

---
+++ codeitsuisse/routes/crypto_collapz.py
@@ -0,0 +1,21 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+from codeitsuisse.challenges import crypto_collapz_challenge
+
+logger = logging.getLogger(__name__)
+
+
+@app.route('/cryptoCollapzTest', methods=['GET'])
+def crypto_collapz_testt():
+    return "Hello, ticker stream is working"
+
+
+@app.route('/cryptocollapz', methods=['POST'])
+def crypto_collapz():
+    data = request.get_json()
+    data = crypto_collapz_challenge.crypto_collapz(data)
+    return jsonify(data)
