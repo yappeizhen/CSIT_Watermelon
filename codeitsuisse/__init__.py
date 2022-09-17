2810f82a55f821e650bf9cc6468b0f3fb69346a4
@menghui.w <menghui.w@affinidi.com>
2022-09-17 15:05:59 +0800

    travelling robot

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -4,5 +4,12 @@ import codeitsuisse.routes.square
 import codeitsuisse.routes.ticker_stream
 import codeitsuisse.routes.crypto_collapz
 import codeitsuisse.routes.calendar_days
+<<<<<<< HEAD
 import codeitsuisse.routes.rubiks
 import codeitsuisse.routes.travelling_suisse_robot
+||||||| parent of eb51000 (travelling robot)
+
+
+=======
+import codeitsuisse.routes.travelling_suisse_robot
+>>>>>>> eb51000 (travelling robot)
