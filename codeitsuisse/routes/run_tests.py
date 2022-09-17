15a1fcd5075e26f3d72d0497fea7dfb8f388eb7f
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 <66234273+yappeizhen@users.noreply.github.com>
2022-09-17 10:14:59 +0800

    Crypto Collapz API and logic

--- codeitsuisse/routes/run_tests.py
+++ codeitsuisse/routes/run_tests.py
@@ -2,6 +2,6 @@
 import unittest
 from main import *
 # Add imports here
-from codeitsuisse.routes.unit_test_cases import to_cumulative, to_cumulative_delayed
+from codeitsuisse.tests.unit_test_cases import to_cumulative, to_cumulative_delayed
 
 
